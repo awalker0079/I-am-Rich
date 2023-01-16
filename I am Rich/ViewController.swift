8ab5f92875f42c0e9540b59ce8b42086f191d8e8
Gene Walker <gene.walker@cfacorp.com>
2023-01-16 12:15:32 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gene Walker on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
